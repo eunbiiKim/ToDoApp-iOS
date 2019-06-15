2e6fa1b579e74beb0d99c42f10b44010870ec341
Jaemyeong Jin <jaemyeong@me.com>
2019-06-15 15:45:23 +0900

    [Close #1] 프로젝트 파일 생성

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by 진재명 on 6/15/19.
+//  Copyright © 2019 Busan iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
