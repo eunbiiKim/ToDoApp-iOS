69c35a3ba3ea9ac734d98ae06a5fe32eed8bf5e4
Jaemyeong Jin <jaemyeong@me.com>
2019-06-29 16:15:30 +0900

    [Close #6] SwiftFormat 설정 추가 (#22)
    * .swift-version 파일 추가
    
    Signed-off-by: Jaemyeong Jin <jaemyeong@me.com>
    
    * .swiftformat 룰 추가
    
    Signed-off-by: Jaemyeong Jin <jaemyeong@me.com>
    
    * 코드 리포맷팅
    
    Signed-off-by: Jaemyeong Jin <jaemyeong@me.com>

--- ToDoTests/ToDoTests.swift
+++ ToDoTests/ToDoTests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2019 Busan iOS. All rights reserved.
 //
 
-import XCTest
 @testable import ToDo
+import XCTest
 
 class ToDoTests: XCTestCase {
-
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -30,5 +29,4 @@ class ToDoTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
